47dc7945be5e344c9a095efe23d2bd611696ef17
maximdushniak <maximdushniak@users.noreply.github.com>
2019-09-04 00:17:04 +0300

    save knock

--- clap_relay.ino
+++ clap_relay.ino
@@ -1,87 +1,61 @@
-#define NOISE_PIN 5     // пин датчика уровня шума
-#define RELAY_PIN 4     // пин рэле
-#define difficulty 100  // миллисекунд на реакцию (плюс минус)
-#define max_knock 5    // число запоминаемых "стуков"
+#define NOISE_PIN 5   // пин датчика уровня шума
+#define RELAY_PIN 4   // пин рэле
+#define REACTION 100  // миллисекунд на реакцию (плюс минус)
+#define MAX_KNOCK 5   // число запоминаемых "стуков"
 
 // включенное состояние системы (1 - вкл., 0 - выкл.)
-bool state_on = false; // система выключена
+// bool state_on = false; // система выключена
 bool write_start = false; // флаг начала записи
 unsigned last_knock = 0; // время последнего хлопка
 // unsigned knock_time;
 
-unsigned wait_time[max_knock], max_wait_time[max_knock], min_wait_time[max_knock];
+unsigned wait_time[MAX_KNOCK], max_wait_time[MAX_KNOCK], min_wait_time[MAX_KNOCK];
 
 volatile byte mode; // 0- ; 1- ; 2-.
 
-byte mode_1 = 0;
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(RELAY_PIN, OUTPUT);
+    pinMode(NOISE_PIN, INPUT);
 
-void setup() {
+    knockWrite();
   
-  Serial.begin(9600);
-  pinMode(RELAY_PIN, OUTPUT);
-  pinMode(NOISE_PIN, INPUT);
-
-  knockWrite();
-
 }
 
 
-void loop() {
-  
-  //bool is_clap = !digitalRead(NOISE_PIN);
+void test_knock() {
 
-  if (mode_1 == 0)
+  if (!digitalRead(NOISE_PIN))
   {
-    Serial.println("Write array:");
-    for (byte i = 0; i < max_knock; i++)
-    {
-      Serial.println(wait_time[i]);
-    }
-    mode_1 = 1;
+    Serial.print(millis());
+    Serial.println(": knock!!!");
   }
   
-  
+  //Serial.println(!digitalRead(NOISE_PIN));
 
-   /* if(is_clap && !state_on) {
-
-    state_on = true;
-    digitalWrite(RELAY_PIN, HIGH);
-    delay(500);
-    
-    Serial.println("ON!");
-        
-    } else if (state_on && is_clap){
-
-
-    state_on = false;
-    digitalWrite(RELAY_PIN, LOW);
-    delay(500);
-    
-    Serial.println("OFF!");
-      
-    } */
-  
 }
 
 
 void knockWrite() { // запись хлопков
 
-  byte knock = 0; // индекс (номер хлопка)  для записи в массив
-  bool is_clap = false;
-  unsigned knock_time;
+  byte knock = 0;       // индекс (номер хлопка)  для записи в массив
+  bool is_clap = false; // флаг удала
+  unsigned knock_time;  // время между хлопками
 
   delay(1000);
-  Serial.println("Write start");
-    
+  Serial.println("GO!");
+
+  delay(500);  
   
   last_knock = millis();
 
   while (1)
   {
-    if (millis() - last_knock > 5000)
+
+    if (millis() - last_knock > 10000)
     {
-      Serial.println("Timeout!");
-      write_start = false;
+      write_start = false; // таймаут, запись остановлена
       break;
     }
     
@@ -89,7 +63,7 @@ void knockWrite() { // запись хлопков
 
     if (is_clap) {
       Serial.println("Start write!!!");
-      write_start = true;
+      write_start = true; // зафиксирован первый удар => начать запись
       last_knock = millis();
       break;
 
@@ -97,32 +71,57 @@ void knockWrite() { // запись хлопков
   }
   
   if (write_start)
-  {
+  { 
+    
     unsigned start_time = last_knock;
     while (1)
     { 
-      delay(50);
+      //delay(50);
       is_clap = !digitalRead(NOISE_PIN);
       if (is_clap)
       {
         knock_time = millis() - last_knock;
+        if (knock_time<50)
+        {
+          continue;
+        }
+        
         wait_time[knock] = knock_time;
         knock++;
         last_knock = millis();
-        is_clap = false;
+        //is_clap = false;
         Serial.println("knock!");
         is_clap = false;
-        if (knock == max_knock || millis()-start_time)
+      
+        if (knock == MAX_KNOCK
+      )
         {
           break;
         }
-        /*if (millis() - last_knock > 3000)
-        {
-          break;
-        } */
+      }
+      if (millis()-start_time > 5000)
+      {
+        break;
       }
     // is_clap = !digitalRead(NOISE_PIN);
     }
     
   }
+
+  Serial.println("Replay..");
+
+  for (int i = 0; i < MAX_KNOCK
+; i++)
+  {
+    Serial.print(i);
+    Serial.print(": ");
+    Serial.println(wait_time[i]);
+  }
+  
+
+}
+
+void loop()
+{
+  //test_knock();
 }
